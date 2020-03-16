d4c6f79aa212fb0db9b45cf82fbfc79724e52e09
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-03-16 18:10:55 +0000

    Old personal project stripped out.

---
+++ Assets/Shaders/Fireball/Sphere/Sphere.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    [Range(2, 256)] public int resolution = 10;
+    [SerializeField, HideInInspector] MeshFilter[] meshFilters;
+    SphereFace[] faces;
+
+    private void OnValidate()
+    {
+        Initialize();
+        GenerateMesh();
+    }
+
+   
+    void Initialize()
+    {
+        if(meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        faces = new SphereFace[6];
+
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                Debug.Log("Mesh Filter: " + i);
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Custom/Fireball"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+
+
+            }
+            faces[i] = new SphereFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    void GenerateMesh()
+    {
+        int sphereCount = 0;
+        foreach (SphereFace sphere in faces)
+        {
+
+            Debug.Log(sphereCount);
+            sphere.CreateMesh();
+        }
+
+    }
+}
---
+++ Assets/Shaders/Fireball/Sphere/SphereFace.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereFace
+{
+    Mesh mesh;
+    int resolution;
+    Vector3 up;
+
+    Vector3 axisA;
+    Vector3 axisB;
+
+    public SphereFace(Mesh mesh, int resolution, Vector3 up)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.up = up;
+
+        axisA = new Vector3(up.y, up.z, up.x);
+        axisB = Vector3.Cross(up, axisA);
+
+    }
+    public void CreateMesh()
+    {
+        Vector3[] verts = new Vector3[resolution * resolution];
+        int[] tri = new int[(resolution - 1) * (resolution - 1) * 6];
+        int triIndex = 0;
+
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int iter = x + y * resolution;
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+                Vector3 pointOnCube = up + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
+                Vector3 pointOnSphere = pointOnCube.normalized;
+                verts[iter] = pointOnSphere;
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    tri[triIndex] = iter;
+                    tri[triIndex + 1] = iter + resolution + 1;
+                    tri[triIndex + 2] = iter + resolution;
+                    
+                    tri[triIndex + 3] = iter;
+                    tri[triIndex + 4] = iter + 1;
+                    tri[triIndex + 5] = iter + resolution + 1;
+                    triIndex += 6;
+                }
+            }
+        }
+        mesh.Clear();
+        mesh.vertices = verts;
+        mesh.triangles = tri;
+        mesh.RecalculateNormals();
+    }
+}
